706bc9b1928fbf7d15c85b266c487a1bd299fa95
Dmitry <94279359+dmitrishkod@users.noreply.github.com>
2022-08-09 20:22:54 +0400

    Add files via upload

---
+++ task-alfabank/src/main/java/com/example/taskalfabank/TaskAlfabankApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskalfabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAlfabankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAlfabankApplication.class, args);
+    }
+
+}
