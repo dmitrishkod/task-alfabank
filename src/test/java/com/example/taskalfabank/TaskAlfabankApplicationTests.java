292b7de986b598eafe3f40aee298231d17964f94
Dmitry <94279359+dmitrishkod@users.noreply.github.com>
2022-08-09 20:24:06 +0400

    Add files via upload

---
+++ src/test/java/com/example/taskalfabank/TaskAlfabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskalfabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAlfabankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
